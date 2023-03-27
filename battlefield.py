cd5ed987396a718350aaf5da3034ea70b991e172
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-03-27 17:16:31 -0400

    Created initial outline for battlefield and updated dinosaur.

---
+++ battlefield.py
@@ -0,0 +1,15 @@
+class Battlefield:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+    
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
--- dinosaur.py
+++ dinosaur.py
@@ -1,7 +1,7 @@
 class Dinosaur:
     def __init__(self, name_passed, attack_power):
         self.name = name_passed
-        self.attack_power = []
+        self.attack_power = attack_power
         self.health = []
     
     def attack_robot(self, robot):
