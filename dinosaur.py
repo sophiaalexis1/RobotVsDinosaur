8f502789a18436ded3554f8968848e1c6638b62f
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-03-27 17:02:37 -0400

    Finished outline for initial dinosaur

--- dinosaur.py
+++ dinosaur.py
@@ -1,5 +1,9 @@
 class Dinosaur:
-    def __init__(self):
-        self.name = ''
+    def __init__(self, name_passed, attack_power):
+        self.name = name_passed
         self.attack_power = []
         self.health = []
+    
+    def attack_robot(self, robot):
+        pass
+    
