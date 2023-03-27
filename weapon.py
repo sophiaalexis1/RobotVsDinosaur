cc67679cfe2683ee7c4e11aaf4aadcb10dde9b0d
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-03-27 17:22:33 -0400

    finished and tested Weapon class

--- weapon.py
+++ weapon.py
@@ -2,3 +2,8 @@ class Weapon:
     def __init__(self, name_passed, attack_power_passed):
         self.name = name_passed
         self.attack_power = attack_power_passed
+
+# Sword = Weapon('Sword',1000)
+
+# print(Sword.name)
+# print(Sword.attack_power)
