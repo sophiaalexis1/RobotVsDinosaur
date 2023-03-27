7086dc56179f5dadbbca4bdf9058012d99d781cd
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-03-27 17:06:53 -0400

    Finished initial Robot outline

---
+++ robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name_passed, weapon_passed):
+        self.name = name_passed
+        self.attack_weapon = weapon_passed
+        self.health = []
+    
+    def attack_dinosaur(self, dinosaur):
+        pass
