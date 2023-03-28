b1975cc7a24bb439f7fd7724e049d7483d7e97c4
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-03-28 11:45:05 -0400

    Added Weapon class as a child class in Robot class

--- robot.py
+++ robot.py
@@ -1,17 +1,21 @@
 from weapon import Weapon
 
-class Robot:
+weapons = ['Excalibur', 'Master_Sword']
+class Robot(Weapon):
     def __init__(self, name_passed):
         self.name = name_passed
-        self.attack_weapon = []
+        self.active_weapon = Weapon()
         self.health = 10000
     
     def attack_dinosaur(self, dinosaur):
-        dinosaur -= self.attack_weapon
+        dinosaur -= self.active_weapon
         print(f'Robot has attacked and has ministered {Weapon} damage to Dinosaur. Robot has {self.health} left.')
+    
+
 
 
 robot_one = Robot('Arthur')
+robot_one.attack_weapon.append(Weapon())
 print(robot_one.name, robot_one.health, robot_one.attack_weapon)
 dinosaur_to_attack = 10000
 
