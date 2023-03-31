21806e6aac54f0339daf3b54997f6217b2a7276f
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-03-30 20:38:43 -0400

    Conditionals for battle phase and winner have been created.

--- battlefield.py
+++ battlefield.py
@@ -1,15 +1,30 @@
-class Battlefield:
+from dinosaur import Dinosaur
+from robot import Robot 
+valid_response = False
+class Battlefield(Robot, Dinosaur):
     def __init__(self):
-        pass
+        self.dinosaur = Dinosaur()
+        self.robot = Robot()
 
     def run_game(self):
-        pass
+        display_welcome(self)
+        battle_phase()
+        display_winner()
     
     def display_welcome(self):
         print('\nWelcome to an epic battle for the ages! \n Only one side can win!\n')
 
     def battle_phase(self):
-        pass
+        while self.robot.health > 0 and self.dinosaur.health > 0:
+            self.robot.attack_dinosaur(Dinosaur)
+            self.dinosaur.attack_robot(Robot)
+        return
 
     def display_winner(self):
-        pass
+        if self.robot.health > 0 and self.dinosaur.health <= 0:
+            print('Robot wins the game!')
+        elif self.robot.health <= 0 and self.dinosaur.health > 0:
+            print('Dinosaur wins the game!')
+        elif self.robot.health <= 0 and self.dinosaur.health <= 0:
+            print('It is a tie!')
+        return
--- dinosaur.py
+++ dinosaur.py
@@ -5,8 +5,8 @@ class Dinosaur:
         self.health = 10000
     
     def attack_robot(self, robot):
-        robot -= self.attack_power
-        print(f'Dinosaur has attacked and has ministered {self.attack_power} damage to Robot. Dinosaur has {self.health} left.')
+        robot.health -= self.attack_power
+        print(f'Dinosaur has attacked and has ministered {self.attack_power} damage to Robot. Dinosaur has {self.health} left. Robot has {robot.health}.')
     
 # t_rex = Dinosaur('T-Rex', 1000)
 # print (t_rex.name, t_rex.attack_power, t_rex.health)
